3315c53d94af08a5246d05569fe0150dc09e024e
tigran-serobyan <tigran1serobyan@gmail.com>
2021-01-15 12:21:06 +0400

    Navigation

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var { getClassrooms, getClassroomByYear } = require('../services/classroom');
 var { getStudentsByClassroom, getStudentById } = require('../services/students');
 var title = '«Մխիթար Սեբաստացի» կրթահամալիրի շրջանավարտներ';
-
+ 
 router.get('/', function (req, res, next) {
-  getClassrooms().then((classrooms) => {
-    res.render('index', { title, classrooms }); 
+  getClassrooms().sort({year: 'desc'}).exec((err, classrooms) => {
+    res.render('index', { title, classrooms });
   });
 });
 
 router.get('/class/:classroom', function (req, res, next) {
   getClassroomByYear(req.params.classroom).then((classroom) => {
-    getStudentsByClassroom(req.params.classroom).then((students) => {
+    getStudentsByClassroom(req.params.classroom).sort({name: 1}).exec((err, students) => {
       res.render('classroom', { title, classroom, students });
     });
   });
@@ -20,7 +20,7 @@ router.get('/class/:classroom', function (req, res, next) {
 
 router.get('/student/:student', function (req, res, next) {
   getStudentById(req.params.student).then((student) => {
-    getClassroomByYear(student.classroom).then((classroom) => {
+    getClassroomByYear(student.class).then((classroom) => {
       res.render('student', { title, classroom, student });
     });
   });
